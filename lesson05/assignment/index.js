293058dd7d0582d0dec6ff6f9fc4ed437ffe9342
ajp84 <prater.anthony.j@gmail.com>
2021-09-04 11:41:40 -0700

    tested js added color variables changed effect colors

---
+++ lesson05/assignment/index.js
@@ -0,0 +1,10 @@
+//Sample JS for testing UI
+const spring = document.getElementById('c3po');
+
+function myfunction(x) {
+  document.getElementById('c3po').style.filter = "invert(0)";
+}
+
+function yourfunction(x) {
+  document.getElementById('c3po').style.filter = "invert(1)";
+}
