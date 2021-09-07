58530f9ed030d7f1b1f7751a9e526e1033d4b8c0
ajp84 <prater.anthony.j@gmail.com>
2021-09-06 19:06:03 -0700

    templated gallery image element

--- lesson06/assignment/index.js
+++ lesson06/assignment/index.js
@@ -1,5 +1,58 @@
-swTheme = new Audio('/sound/Far Out - The Force Theme.mp3');
+const imageWrapper = document.querySelector(".wrapper_gallery");
 
-swTheme.addEventListener("canplaythrough", event => {
-  /* the audio is now playable; play it if permissions allow */
-  swTheme.play();
+// function request(){
+// fetch("https://api.unsplash.com/photos/?client_id=PaHrfu4_q3NMgYkSbeKWbwn7QLzd8AQuzUtbVnBkafg",function(data){
+//   console.log(response);
+// })
+// }
+
+
+/*Fetch function for JSON data*/
+async function fetchDataJSON() {
+  const response = await fetch('https://api.unsplash.com/photos/?client_id=PaHrfu4_q3NMgYkSbeKWbwn7QLzd8AQuzUtbVnBkafg');
+  const data = await response.json();  return data;
+};
+
+fetchDataJSON().then(data => {
+  data; // fetched data
+
+  console.log(data);
+
+
+/*Loop Here*/
+data.forEach((data) => {
+
+  let imageBox = document.createElement('figure');
+  imageBox.className = 'gallery__item col-lg-4 col-md-6 col-sm-12';
+  imageBox.id = 'gallery';
+  let imageLink = data.urls.regular;
+  let creditLink = data.user.links.html;
+  let creditName = data.user.name;
+
+  imageBox.innerHTML =`
+    <a href="#" class="d-block text-center">
+      <img class="img-fluid img-thumbnail bg-dark gallery__image" src="${imageLink}" alt="star wars inspired image">
+    </a>
+    <br>
+    <p class="m-auto text-light">Credit to <a href="${creditLink}">${creditName}</a></p>
+`
+imageWrapper.appendChild(imageBox)
+
+});
+});
+
+//
+// for (let pic = 0; pic < 13; pic++) {
+//   // Runs 13 times, with values of step 0 through 12.
+//   // let imageLink = 'https://source.unsplash.com/1600x900/?star wars'
+//   let imageBox = document.getElementById("gallery")
+//   imageBox.HTML =  `
+//   <a href="#" class="d-block w-100">
+//      <img class="img-fluid img-thumbnail bg-dark" src="${imageLink}" alt="star wars inspired image">
+//    </a>
+// `
+// imageWrapper.appendChild(imageBox);
+//
+//
+//   console.log('here');
+// };
