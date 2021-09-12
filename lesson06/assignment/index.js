033532ec3305da7f2eb533b4f77481ee01868641
ajp84 <prater.anthony.j@gmail.com>
2021-09-11 22:57:07 -0700

    added images to folder and spacing to elements

--- lesson06/assignment/index.js
+++ lesson06/assignment/index.js
@@ -41,3 +41,14 @@ fetchDataJSON().then(data => {
 
   });
 });
+
+//Sample JS for testing UI
+const spring = document.getElementById('c3po');
+
+function myfunction(x) {
+  document.getElementById('c3po').style.filter = "invert(0)";
+}
+
+function yourfunction(x) {
+  document.getElementById('c3po').style.filter = "invert(1)";
+}
