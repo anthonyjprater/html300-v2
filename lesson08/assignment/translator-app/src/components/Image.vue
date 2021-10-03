53a109934a63c912c2be9c8c6c4e7b533498b0e3
ajp84 <prater.anthony.j@gmail.com>
2021-10-03 15:14:25 -0700

    added prop validation

--- lesson08/assignment/translator-app/src/components/Image.vue
+++ lesson08/assignment/translator-app/src/components/Image.vue
@@ -13,7 +13,18 @@ export default {
     }
   },
   props: {
-
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
   },
   methods: {
     doSomething() {
